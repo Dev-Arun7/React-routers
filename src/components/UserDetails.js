51ef96996875b70a4a2fdd712d62bbe9a42e280b
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2024-07-25 10:42:44 +0530

    v1.60 useParams added

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,8 +1,12 @@
-
+// User details page
+import { useParams } from 'react-router-dom'
 function UserDetails() {
+    const params = useParams()
+    const userId = params.userId
     return (
         <>
             <h2> User Details </h2>
+            <h3> User ID: {userId}</h3>
         </>
     )
 }
